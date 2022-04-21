20d1e6fd1a5cd9bf3a4474cf303ec48e5f75ed3c
Ushanii <ushanisdahanayake@gmail.com>
2022-04-21 12:40:07 +0530

    Payment format

--- ElectroGrid/src/main/java/com/jersey/bean/Payment.java
+++ ElectroGrid/src/main/java/com/jersey/bean/Payment.java
@@ -6,26 +6,26 @@ import com.jersey.dbconn.DbConnectionProvider;
 
 public class Payment {
 
-	
-	public String insertPayment(String account, String amount,String date) {
+	public String insertPayment(String account, String amount, String date) {
 		String output = "";
 		try {
-			Connection con = DbConnectionProvider.getConnection();;
+			Connection con = DbConnectionProvider.getConnection();
+			;
 			if (con == null) {
 				return "Error while connecting to the database for inserting.";
 			}
 			// create a prepared statement
-			String query = " insert into payment(`paymentId`,`accountNum`,`amount`,`date`)"+ " values (?, ?, ?, ?)";
+			String query = " insert into payment(`paymentId`,`accountNum`,`amount`,`date`)" + " values (?, ?, ?, ?)";
 			PreparedStatement preparedStmt = con.prepareStatement(query);
 			// binding values
 			preparedStmt.setInt(1, 0);
-			preparedStmt.setString(2, account);			
+			preparedStmt.setString(2, account);
 			preparedStmt.setDouble(3, Double.parseDouble(amount));
 			preparedStmt.setString(4, date);
-			
+
 			// execute the statement
 			preparedStmt.execute();
-			//con.close();
+			// con.close();
 			output = "Inserted successfully";
 		} catch (Exception e) {
 			output = "Error while inserting the item.";
@@ -33,8 +33,7 @@ public class Payment {
 		}
 		return output;
 	}
-	
-	
+
 	public String readPayment() {
 		String output = "";
 		try {
@@ -43,27 +42,30 @@ public class Payment {
 				return "Error while connecting to the database for reading.";
 			}
 			// Prepare the html table to be displayed
-			output = "<table border='1'><tr><th>Payment ID</th>" + "<th>Account Number</th>"+ "<th>Payment Amount</th>" + "<th>Payment Date</th>";
+			output = "<table border='1'><tr><th>Payment ID</th>" + "<th>Account Number</th>" + "<th>Payment Amount</th>"
+					+ "<th>Payment Date</th>";
 			String query = "select * from payment";
 			Statement stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery(query);
 			// iterate through the rows in the result set
 			while (rs.next()) {
 				String paymentId = Integer.toString(rs.getInt("paymentId"));
-				String accountNum = rs.getString("accountNum");				
+				String accountNum = rs.getString("accountNum");
 				String amount = Double.toString(rs.getDouble("amount"));
 				String date = rs.getString("date");
-				
+
 				// Add into the html table
 				output += "<tr><td>" + paymentId + "</td>";
 				output += "<td>" + accountNum + "</td>";
 				output += "<td>" + amount + "</td>";
 				output += "<td>" + date + "</td>";
-				
-				// buttons				
-				output += "<td><form method='post' action='PaymentForm.jsp'>"+ "<input name='btnRemove' type='submit' value='Remove'class='btn btn-danger'>"+ "<input name='paymentId' type='hidden' value='" + paymentId + "'>" + "</form></td></tr>";
+
+				// buttons
+				output += "<td><form method='post' action='PaymentForm.jsp'>"
+						+ "<input name='btnRemove' type='submit' value='Remove'class='btn btn-danger'>"
+						+ "<input name='paymentId' type='hidden' value='" + paymentId + "'>" + "</form></td></tr>";
 			}
-			//con.close();
+			// con.close();
 			// Complete the html table
 			output += "</table>";
 		} catch (Exception e) {
@@ -72,10 +74,8 @@ public class Payment {
 		}
 		return output;
 	}
-	
-	
-	
-	public String updatePayment(String ID, String account, String amount,String date)
+
+	public String updatePayment(String ID, String account, String amount, String date)
 
 	{
 		String output = "";
@@ -88,13 +88,13 @@ public class Payment {
 			String query = "UPDATE payment SET accountNum=?,amount=?,date=?WHERE paymentId=?";
 			PreparedStatement preparedStmt = con.prepareStatement(query);
 			// binding values
-			preparedStmt.setString(1, account);			
+			preparedStmt.setString(1, account);
 			preparedStmt.setDouble(2, Double.parseDouble(amount));
-			preparedStmt.setString(3, date);			
+			preparedStmt.setString(3, date);
 			preparedStmt.setInt(4, Integer.parseInt(ID));
 			// execute the statement
 			preparedStmt.execute();
-			//con.close();
+			// con.close();
 			output = "Updated successfully";
 		} catch (Exception e) {
 			output = "Error while updating the payment.";
@@ -102,8 +102,7 @@ public class Payment {
 		}
 		return output;
 	}
-	
-	
+
 	public String deletePayment(String paymentId) {
 		String output = "";
 		try {
@@ -118,7 +117,7 @@ public class Payment {
 			preparedStmt.setInt(1, Integer.parseInt(paymentId));
 			// execute the statement
 			preparedStmt.execute();
-			//con.close();
+			// con.close();
 			output = "Deleted successfully";
 		} catch (Exception e) {
 			output = "Error while deleting the payment.";
@@ -126,5 +125,5 @@ public class Payment {
 		}
 		return output;
 	}
-	
+
 }
