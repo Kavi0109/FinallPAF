3a19d7c4a3d34135dda8a0423bd021ca080525ed
Ushanii <ushanisdahanayake@gmail.com>
2022-04-21 12:32:30 +0530

    Payment commit

--- ElectroGrid/src/main/java/com/jersey/dbconn/DbConnectionInfo.java
+++ ElectroGrid/src/main/java/com/jersey/dbconn/DbConnectionInfo.java
@@ -5,5 +5,5 @@ public interface DbConnectionInfo
 	public static final String DRIVER_NAME="com.mysql.cj.jdbc.Driver";
 	public static final String DRIVER_URL="jdbc:mysql://localhost:3306/paf";
 	public static final String USER_NAME="root";
-	public static final String USER_PASS="";
+	public static final String USER_PASS="mysql";
 }
