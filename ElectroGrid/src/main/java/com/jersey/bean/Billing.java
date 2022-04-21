858acbc692bc044710e729b93c15bf48b6dcd199
Rishma Packeeran <it20041298@my.sliit.lk>
2022-04-21 15:32:04 +0530

    Connection

--- ElectroGrid/src/main/java/com/jersey/bean/Billing.java
+++ ElectroGrid/src/main/java/com/jersey/bean/Billing.java
@@ -5,24 +5,6 @@ import com.jersey.dbconn.DbConnectionProvider;
 
 public class Billing {
 	
-	//A common method to connect to the DB
-	/*private Connection connect() {
-		Connection con = null;
-		try
-		{
-			Class.forName("com.mysql.jdbc.Driver");
-			
-			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/paf","root","");
-		}
-		
-		catch(Exception e){
-			e.printStackTrace();
-		}
-		return con;
-	}*/
-	
-	
-	
 	
 	//Retrieve Bills
 		public String readBills()
