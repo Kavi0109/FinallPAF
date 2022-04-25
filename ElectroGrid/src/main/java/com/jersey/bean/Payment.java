896ad81578c7f8dc9bb5a0196857846d6576b3c4
Ushanii <ushanisdahanayake@gmail.com>
2022-04-25 19:23:50 +0530

    Payment search

--- ElectroGrid/src/main/java/com/jersey/bean/Payment.java
+++ ElectroGrid/src/main/java/com/jersey/bean/Payment.java
@@ -6,143 +6,247 @@ import com.jersey.dbconn.DbConnectionProvider;
 
 public class Payment {
 
+	// insert payment details
+	
 	public String insertPayment(String account, String amount, String date) {
+		
 		String output = "";
+		
 		try {
-			Connection con = DbConnectionProvider.getConnection();
 			
+			// database connection
+			Connection con = DbConnectionProvider.getConnection();
+		
+			// error while connecting
 			if (con == null) {
 				return "Error while connecting to the database for inserting.";
 			}
-			
+		
 			// create a prepared statement
 			String query = " insert into payment(`paymentId`,`accountNum`,`amount`,`date`)" + " values (?, ?, ?, ?)";
 			PreparedStatement preparedStmt = con.prepareStatement(query);
-			
+		
 			// binding values
 			preparedStmt.setInt(1, 0);
 			preparedStmt.setString(2, account);
 			preparedStmt.setDouble(3, Double.parseDouble(amount));
 			preparedStmt.setString(4, date);
-
+		
 			// execute the statement
 			preparedStmt.execute();
 			
-			output = "Inserted successfully";
+			// insert success message
+			output = "Inserted Successfully!";
+		
+		}
+		catch (Exception e) {
 			
-		} catch (Exception e) {
-			output = "Error while inserting the item.";
+			// error while inserting
+			output = "Error while inserting payment details!";
 			System.err.println(e.getMessage());
 		}
+		
 		return output;
 	}
-	
 
+		
+	// read payment details
+	
 	public String readPayment() {
+		
 		String output = "";
+		
 		try {
 			
+			// database connection
 			Connection con = DbConnectionProvider.getConnection();
+			
+			// error while connecting
 			if (con == null) {
 				return "Error while connecting to the database for reading.";
 			}
-			
+
 			// Prepare the html table to be displayed
-			output = "<table border='1'><tr><th>Payment ID</th>" + "<th>Account Number</th>" + "<th>Payment Amount</th>" + "<th>Payment Date</th>";
-			
+			output = "<table border='1'><tr><th>Payment ID</th>" + "<th>Account Number</th>"
+						+ "<th>Payment Amount</th>" + "<th>Payment Date</th>" + "<th>Delete</th>";
+
 			String query = "select * from payment";
 			Statement stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery(query);
-			
+
 			// iterate through the rows in the result set
 			while (rs.next()) {
+				
 				String paymentId = Integer.toString(rs.getInt("paymentId"));
 				String accountNum = rs.getString("accountNum");
 				String amount = Double.toString(rs.getDouble("amount"));
 				String date = rs.getString("date");
 
-				// Add into the html table
+				// add into the html table
 				output += "<tr><td>" + paymentId + "</td>";
 				output += "<td>" + accountNum + "</td>";
 				output += "<td>" + amount + "</td>";
 				output += "<td>" + date + "</td>";
 
 				// buttons
-				output += "<td><form method='post' action='PaymentForm.jsp'>"
-						+ "<input name='btnRemove' type='submit' value='Remove'class='btn btn-danger'>"
+				output += "<td><input name='btnRemove' type='submit' value='Remove'class='btn btn-danger'>"
 						+ "<input name='paymentId' type='hidden' value='" + paymentId + "'>" + "</form></td></tr>";
 			}
-			
+
 			// Complete the html table
 			output += "</table>";
+
+		}
+		catch (Exception e) {
 			
-		} catch (Exception e) {
-			output = "Error while reading the items.";
+			// error while reading
+			output = "Error while reading payment details!";
 			System.err.println(e.getMessage());
 		}
+		
 		return output;
 	}
+
+		
 	
-
-	public String updatePayment(String ID, String account, String amount, String date)
-
-	{
-		String output = "";
+	// update payment details
+	
+	public String updatePayment(String ID, String account, String amount, String date) {
+		
+		String output = "";		
+		
 		try {
+			
+			// database connection
 			Connection con = DbConnectionProvider.getConnection();
+			
 			if (con == null) {
 				return "Error while connecting to the database for updating.";
 			}
-			
+
 			// create a prepared statement
 			String query = "UPDATE payment SET accountNum=?,amount=?,date=?WHERE paymentId=?";
 			PreparedStatement preparedStmt = con.prepareStatement(query);
-			
+
 			// binding values
 			preparedStmt.setString(1, account);
 			preparedStmt.setDouble(2, Double.parseDouble(amount));
 			preparedStmt.setString(3, date);
 			preparedStmt.setInt(4, Integer.parseInt(ID));
-			
+
 			// execute the statement
 			preparedStmt.execute();
-			// con.close();
-			output = "Updated successfully";
 			
-		} catch (Exception e) {
-			output = "Error while updating the payment.";
+			// update success message
+			output = "Updated successfully!";
+
+		}
+		catch (Exception e) {
+			
+			// error while updating
+			output = "Error while updating the payment!";
 			System.err.println(e.getMessage());
 		}
+		
 		return output;
 	}
 	
-
+	
+	// delete payment details
+	
 	public String deletePayment(String paymentId) {
+		
 		String output = "";
+		
 		try {
+			
+			// database connection
 			Connection con = DbConnectionProvider.getConnection();
+			
 			if (con == null) {
 				return "Error while connecting to the database for deleting.";
 			}
-			
+	
 			// create a prepared statement
 			String query = "delete from payment where paymentId=?";
 			PreparedStatement preparedStmt = con.prepareStatement(query);
-			
+	
 			// binding values
 			preparedStmt.setInt(1, Integer.parseInt(paymentId));
-			
+	
 			// execute the statement
 			preparedStmt.execute();
+	
+			// delete success message
+			output = "Deleted successfully!";
+	
+		}
+		catch (Exception e) {
 			
-			output = "Deleted successfully";
-			
-		} catch (Exception e) {
-			output = "Error while deleting the payment.";
+			// error while deleting
+			output = "Error while deleting the payment!";
 			System.err.println(e.getMessage());
 		}
+		
+		return output;
+	}	
+	
+	
+	// search payment details
+	
+	public String searchPayment(String id) {
+	
+		int convertedID = Integer.parseInt(id);
+		String output = "";
+		
+		try {
+			
+			// database connection
+			Connection con = DbConnectionProvider.getConnection();
+			
+			if (con == null) {
+				return "Error while connecting to the database for searching.";
+			}
+			
+			// Prepare the html table to be displayed
+			output = "<table border='1'><tr><th>Payment ID</th>" + "<th>Account Number</th>"
+						+ "<th>Payment Amount</th>" + "<th>Payment Date</th>" + "<th>Delete</th>";
+	
+			String query = "select * from payment where accountNum='" + convertedID + "'";
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			
+			// iterate through the rows in the result set
+			while (rs.next()) {
+				
+				String paymentId = Integer.toString(rs.getInt("paymentId"));
+				String accountNum = rs.getString("accountNum");
+				String amount = Double.toString(rs.getDouble("amount"));
+				String date = rs.getString("date");
+	
+				// add into the html table
+				output += "<tr><td>" + paymentId + "</td>";
+				output += "<td>" + accountNum + "</td>";
+				output += "<td>" + amount + "</td>";
+				output += "<td>" + date + "</td>";
+	
+				// buttons
+				output += "<td><input name='btnRemove' type='submit' value='Remove'class='btn btn-danger'>"
+						+ "<input name='paymentId' type='hidden' value='" + paymentId + "'>" + "</form></td></tr>";
+			}
+	
+			// Complete the html table
+			output += "</table>";
+			
+		}
+		catch (Exception e) {
+			
+			// error while searching
+			output = "Error while searching payment details!";
+			System.err.println(e.getMessage());
+		}
+		
 		return output;
 	}
-	
 
 }
--- ElectroGrid/src/main/java/com/jersey/services/PaymentService.java
+++ ElectroGrid/src/main/java/com/jersey/services/PaymentService.java
@@ -7,6 +7,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -23,32 +24,37 @@ public class PaymentService {
 
 	Payment payObj = new Payment();
 
+	// read payment details
 	@GET
 	@Path("/get")
-	@Produces(MediaType.TEXT_PLAIN)
-	public String readPayment() {
+	@Produces(MediaType.TEXT_HTML)
+	public String readPayment() {		
 		return payObj.readPayment();
 	}
 
+	// insert payment details
 	@POST
 	@Path("/post")
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces(MediaType.TEXT_PLAIN)
 	public String insertPayment(@FormParam("accountNum") String accountNum,
 								@FormParam("amount") String amount,
-								@FormParam("date") String date) {
-		String output = payObj.insertPayment(accountNum, amount, date);
-		return output;
-	}
+								@FormParam("date") String date) {						
+								String output = payObj.insertPayment(accountNum, amount, date);
+								return output;
+							}
 
+	// update payment details
 	@PUT
 	@Path("/put")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.TEXT_PLAIN)
 	public String updatePayment(String paymentData) {
-		// Convert the input string to a JSON object
+		
+		// convert the input string to a JSON object
 		JsonObject paymentObject = new JsonParser().parse(paymentData).getAsJsonObject();
-		// Read the values from the JSON object
+		
+		// read the values from the JSON object
 		String paymentId = paymentObject.get("paymentId").getAsString();
 		String accountNum = paymentObject.get("accountNum").getAsString();
 		String amount = paymentObject.get("amount").getAsString();
@@ -58,17 +64,29 @@ public class PaymentService {
 		return output;
 	}
 
+	// delete payment details
 	@DELETE
 	@Path("/delete")
 	@Consumes(MediaType.APPLICATION_XML)
 	@Produces(MediaType.TEXT_PLAIN)
 	public String deletePayment(String paymentData) {
-		// Convert the input string to an XML document
+		
+		// convert the input string to an XML document
 		Document doc = Jsoup.parse(paymentData, "", Parser.xmlParser());
-		// Read the value from the element <itemID>
+		
+		// read the value from the element <paymentId>
 		String paymentId = doc.select("paymentId").text();
 		String output = payObj.deletePayment(paymentId);
 		return output;
 	}
+	
+	// search payment details
+	@GET
+	@Path("search/{id}")
+	@Produces(MediaType.TEXT_HTML)
+	public String searchPayment(@PathParam("id") String id) 
+	 {
+		return payObj.searchPayment(id); 
+	 }
 
 }
